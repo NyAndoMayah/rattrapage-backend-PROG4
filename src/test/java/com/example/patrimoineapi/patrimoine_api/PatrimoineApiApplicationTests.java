65dd140ffd3981d12802906e706106af9f1c58b5
NyAndoMayah <hei.mayah.3@gmail.com>
2024-09-23 13:37:51 +0300

    feat: initialize project

---
+++ src/test/java/com/example/patrimoineapi/patrimoine_api/PatrimoineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patrimoineapi.patrimoine_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
